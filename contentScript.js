67f0fab53e06a51c90761d5fa5ca66b5cd753a6f
Sander Vispoel <sandervispoel@hotmail.com>
2023-07-25 12:11:43 +0200

    Add dark mode support

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,9 @@
+const TWITTER_BLUE_RGB = "rgb(29, 155, 240)";
+let darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+let isDarkmode = darkModePreference.matches;
+let navbarIcon;
+let changed = false;
+
 // This function changes the favicon to the specified image URL
 function changeFavicon(newFaviconUrl) {
   const oldFavicon = document.querySelector('link[rel="shortcut icon"]');
@@ -16,20 +22,12 @@ function appendSVGToElement(element, svgString) {
   const svg = parser
     .parseFromString(svgString, "image/svg+xml")
     .querySelector("svg");
-  console.log(svg);
-  element.appendChild(svg);
+
+  navbarIcon = element.appendChild(svg);
 }
 
-const iconSvg = (classname) => `
-<svg class="${classname}" viewBox="0 0 48 40" id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:white;stroke:none;stroke-linecap:round;stroke-linejoin:round;}</style></defs><path class="cls-1" d="M38.74,16.55v1c0,10.07-7.64,21.61-21.62,21.61A21.14,21.14,0,0,1,5.5,35.71a12.22,12.22,0,0,0,1.81.11,15.25,15.25,0,0,0,9.44-3.24,7.56,7.56,0,0,1-7.1-5.29,6.9,6.9,0,0,0,1.44.15,7.53,7.53,0,0,0,2-.27A7.57,7.57,0,0,1,7,19.72v-.1a7.42,7.42,0,0,0,3.44.94A7.54,7.54,0,0,1,8.05,10.5a21.58,21.58,0,0,0,15.68,7.94,6.38,6.38,0,0,1-.21-1.74,7.55,7.55,0,0,1,13.17-5.31,15.59,15.59,0,0,0,4.83-1.85,7.65,7.65,0,0,1-3.39,4.27,15.87,15.87,0,0,0,4.37-1.26,15.56,15.56,0,0,1-3.76,4Z"/></svg>
-`;
-
-const faviconPath = chrome.runtime.getURL("favicon.png");
-changeFavicon(faviconPath);
-
-let changed = false;
-
-const itvId = setInterval(() => {
+function changeIcons() {
+  console.log(1);
   const topLeftIcon = document.querySelector('a[aria-label="Twitter"] svg');
 
   if (topLeftIcon) {
@@ -57,4 +55,27 @@ const itvId = setInterval(() => {
     // Delete icon from DOM
     loadingIcon.parentNode.removeChild(loadingIcon);
   }
-}, 100);
+}
+
+const iconSvg = (classname) => {
+  const fill = isDarkmode ? "white" : TWITTER_BLUE_RGB;
+
+  return `
+<svg class="${classname}" viewBox="0 0 48 40" id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:${fill};stroke:none;stroke-linecap:round;stroke-linejoin:round;}</style></defs><path class="cls-1" d="M38.74,16.55v1c0,10.07-7.64,21.61-21.62,21.61A21.14,21.14,0,0,1,5.5,35.71a12.22,12.22,0,0,0,1.81.11,15.25,15.25,0,0,0,9.44-3.24,7.56,7.56,0,0,1-7.1-5.29,6.9,6.9,0,0,0,1.44.15,7.53,7.53,0,0,0,2-.27A7.57,7.57,0,0,1,7,19.72v-.1a7.42,7.42,0,0,0,3.44.94A7.54,7.54,0,0,1,8.05,10.5a21.58,21.58,0,0,0,15.68,7.94,6.38,6.38,0,0,1-.21-1.74,7.55,7.55,0,0,1,13.17-5.31,15.59,15.59,0,0,0,4.83-1.85,7.65,7.65,0,0,1-3.39,4.27,15.87,15.87,0,0,0,4.37-1.26,15.56,15.56,0,0,1-3.76,4Z"/></svg>
+`;
+};
+
+const faviconPath = chrome.runtime.getURL("favicon.png");
+changeFavicon(faviconPath);
+
+let itvId = setInterval(changeIcons, 10);
+
+darkModePreference.addEventListener("change", (e) => {
+  isDarkmode = e.matches;
+  changed = false;
+  itvId = setInterval(changeIcons, 10);
+
+  if (navbarIcon) {
+    navbarIcon.style.fill = isDarkmode ? "white" : "TWITTER_BLUE_RGB";
+  }
+});
